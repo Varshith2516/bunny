44e2417900fc9c508b5eab1d39bcc353ba54bc5d
Varshith2516 <himavarshith5678@gmail.com>
2026-03-09 18:00:15 +0530

    Add Employee class with documentId and employeeName

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+using System;
+ 
+namespace SecureDocs
+
+{
+
+    public class Employee
+
+    {
+
+        public string documentId;
+
+        public string employeeName;
+ 
+        public Employee(string documentId, string employeeName)
+
+        {
+
+            this.documentId = documentId;
+
+            this.employeeName = employeeName;
+
+        }
+
+    }
+
+}
+ 
